c1587a0d43a6c41b95ea769d4fb8e027d51857a4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-14 19:43:32 -0500

    Sort the stacks by the mem peaks

--- fg_mem.sh
+++ fg_mem.sh
@@ -5,5 +5,5 @@ echo "Executing: $@"
 heaptrack -o heaptrack_out "$@"
 
 echo "Generating Memory FlameGraph"
-heaptrack_print -f heaptrack_out.gz -a  --flamegraph-cost-type peak -F stacks.txt
+heaptrack_print -f heaptrack_out.gz -p  --flamegraph-cost-type peak -F stacks.txt
 /tmp/FlameGraph/flamegraph.pl --title "heaptrack: peak" --colors mem --countname peak < stacks.txt > fg_mem.svg

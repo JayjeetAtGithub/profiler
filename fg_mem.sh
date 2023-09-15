f996db2c28cf38e856c3ba6eb770f75f79f9dc2b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-14 19:31:36 -0500

    Fixes

--- fg_mem.sh
+++ fg_mem.sh
@@ -6,4 +6,4 @@ heaptrack -o heaptrack_out "$@"
 
 echo "Generating Memory FlameGraph"
 heaptrack_print -f heaptrack_out.gz -p -F stacks.txt
-/tmp/FlameGraph/flamegraph.pl --title "heaptrack" --colors mem --countname peak < stacks.txt > fg_mem.svg
+/tmp/FlameGraph/flamegraph.pl --title "heaptrack" --colors mem --countname allocations < stacks.txt > fg_mem.svg

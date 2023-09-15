4409368f89f1c5835605809da6e025a1f4203836
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-14 19:08:07 -0500

    Add code to generate cpu flame graph

---
+++ fg_cpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Executing: $@"
+perf record -F 99 -a -g -- "$@"
+
+echo "Generating CPU FlameGraph"
+perf script | /tmp/FlameGraph/stackcollapse-perf.pl > out.perf-folded
+/tmp/FlameGraph/flamegraph.pl out.perf-folded > perf.svg

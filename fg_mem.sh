0962ea13613d58d7c133cc33a99285bec2b8c0f4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-14 19:44:32 -0500

    Update mem script

--- fg_mem.sh
+++ fg_mem.sh
@@ -4,6 +4,6 @@ set -e
 echo "Executing: $@"
 heaptrack -o heaptrack_out "$@"
 
-echo "Generating Memory FlameGraph"
+echo "Generating Memory Peak FlameGraph"
 heaptrack_print -f heaptrack_out.gz -p  --flamegraph-cost-type peak -F stacks.txt
 /tmp/FlameGraph/flamegraph.pl --title "heaptrack: peak" --colors mem --countname peak < stacks.txt > fg_mem.svg

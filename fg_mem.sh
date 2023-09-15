336b6aaaf1ecbbcdd2f49e0f2ca269a73118dbb4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2023-09-14 19:22:11 -0500

    Replace with -o

--- fg_mem.sh
+++ fg_mem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-heaptrack --output heaptrack.gz "$@"
+heaptrack -o heaptrack.gz "$@"
 heaptrack_print -f heaptrack.gz -p -F stacks.txt
 /tmp/FlameGraph/flamegraph.pl --title "heaptrack" --colors mem --countname peak < stacks.txt > fg_mem.svg
